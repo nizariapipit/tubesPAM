e0430afcc66e5f73f0478aa3b11a086309384664
pipit nizaria <87574829+nizariapipit@users.noreply.github.com>
2022-05-31 19:44:41 +0700

    Update ApiService.kt

--- app/src/main/java/tugas/besar/tubespam/retrofit/ApiService.kt
+++ app/src/main/java/tugas/besar/tubespam/retrofit/ApiService.kt
@@ -4,8 +4,7 @@ import retrofit2.Call
 import retrofit2.http.*
 
 interface ApiService {
-    @Headers("Authorization: Bearer 563492ad6f917000010000019a72c8387a094d11809956043350720c")
-    //@GET("/v1/search")
+    @Headers("Authorization: Bearer <token>")
     @GET("/v1/search")
     fun searchQueryGet(
         @Query("query") query: String
